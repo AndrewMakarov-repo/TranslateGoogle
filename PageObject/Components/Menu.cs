049acb11f9ac7f3c18e36fb3042a363938ae03a6
Andrey Makarov <andremkr@tut.by>
2022-07-29 18:35:35 +0300

    New locators and new "Menu"

--- PageObject/Components/Menu.cs
+++ PageObject/Components/Menu.cs
@@ -1,4 +1,5 @@
-﻿using OpenQA.Selenium;
+﻿using Core.Utilities.Extentions;
+using OpenQA.Selenium;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,22 +15,100 @@ namespace PageObjects.Components
         //locator for left and right drop-down in quick menu "//c-wiz[@data-node-index='3;0']//c-wiz[@data-node-index='1;0']/div/button"
 
         private IWebDriver driver;
+        private IWebElement rootElement;
 
-        private static readonly By SelectedSourceElementLocator = By.XPath("(//div[@role='tablist'])[1]//button[@aria-selected='true']");
-        private static readonly By SelectedTargetElementLocator = By.XPath("(//div[@role='tablist'])[2]//button[@aria-selected='true']");
-        private readonly By SelectedLanguageLocator;
+        private static readonly By SelectedLanguageLocator = By.XPath(".//button[@aria-selected='true']");
+        //private static readonly By QuickTargetMenuButtonsLocator = By.XPath("(//div[@role='tablist'])[2]//button[@aria-selected='false']");
+        //private static readonly By QuickSourceMenuButtonsLocator = By.XPath("(//div[@role='tablist'])[1]//button[@aria-selected='false']");
+        //private static readonly By DropTargetDownMenuButtonLocator = By.XPath("//button[@aria-label='More target languages']");
+        //private static readonly By DropSourceDownMenuButtonLocator = By.XPath("//button[@aria-label='More source languages']");
+        //private static string SelectedTargetElementFromDropDownLocator = "//div[@data-language-code]/div[text()='{0}']";
+        //private static string SelectedSourceElementFromDropDownLocator = "//div[@data-language-code]/div[text()='{0}']";
+
+
+
+        private static readonly By QuickMenuButtonsLocator = By.XPath(".//button[@aria-selected='false']");
+        private static readonly By DropDownMenuButtonLocator = By.XPath(".//button[contains(@aria-label,'More')]");
+        private static string SelectedElementFromDropDownLocator = "//div[@data-language-code]/div[text()='{0}']";
+
+        //ctor
+        public Menu(IWebDriver driver, IWebElement rootElement)
+        {
+            this.driver = driver;
+            this.rootElement = rootElement;
+        }
+
+        //ctor2
+        public Menu(IWebDriver driver, By rootElementLocator)
+        {
+            this.driver = driver;
+            this.rootElement = driver.FindElement(rootElementLocator);
+        }
+
 
         public string SelectedLanguage 
         {
             get
             {
-                var selectedElement = driver.FindElement(SelectedLanguageLocator);
+                var selectedElement = rootElement.FindElement(SelectedLanguageLocator);
                 return selectedElement.Text;
             } 
         }
 
         public void SelectLanguage(string language)
         {
+            if (SelectedLanguage == language)
+            {
+                return;
+            }
+
+            if (CheckQuickMenu(language))
+            {
+                SelectQuickMenu(language);
+            }
+            else
+            {
+                SelectLanguageFromMoreMenu(language);
+            }
+        }
+
+        private void SelectLanguageFromMoreMenu(string language)
+        {
+            var dropDownMenuButtons = this.rootElement.FindElements(DropDownMenuButtonLocator);
+            var firstDropDownMenuButton = dropDownMenuButtons.FirstOrDefault(x => x.Displayed);
+            if (firstDropDownMenuButton != null)
+            {
+                //Actions action = new Actions(driver);
+                //action.MoveToElement((IWebElement)firstDropDownMenuButton).Click().Perform();
+                firstDropDownMenuButton.Click();
+            }
+            else
+            {
+                throw new Exception("Drop-down menu is not found");
+            }
+
+            var selectedSourceElementFromDropDown = this.rootElement.FindElements(By.XPath(string.Format(SelectedElementFromDropDownLocator, language)));
+            var selectedLanguageFromDropDown = selectedSourceElementFromDropDown.Where(x => x.Displayed).FirstOrDefault();
+
+            driver.HoverAndClick(selectedLanguageFromDropDown);
+
+        }
+
+        private void SelectQuickMenu(string language)
+        {
+            var quickMenuButtons = this.rootElement.FindElements(QuickMenuButtonsLocator);
+            var possibleElement = quickMenuButtons.Where(x => string.Equals(x.Text, language, StringComparison.OrdinalIgnoreCase)).FirstOrDefault();
+            if (possibleElement != null)
+            {
+                possibleElement.Click();
+            }
+        }
+
+        private bool CheckQuickMenu(string language)
+        {
+            var quickMenuButtons = this.rootElement.FindElements(QuickMenuButtonsLocator);
+            var possibleElement = quickMenuButtons.Where(x => string.Equals(x.Text, language, StringComparison.OrdinalIgnoreCase)).FirstOrDefault();
+            return possibleElement != null;
         }
     }
 }
